6553f334c439c111600f34ae3201fc68363fd144
s-prechtl <stefan@tague.at>
2022-03-04 09:03:37 +0100

    Teil von Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File f = new File("src/com/company/saves.CSV");
+        FileReader fr = new FileReader(f);
+        BufferedReader bufferedReader = new BufferedReader(fr);
+        ArrayList<Date> dates = new ArrayList<>();
 
-        System.out.println("Hello World");
+        bufferedReader.readLine();
+
 
     }
 }
