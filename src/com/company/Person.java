c65aa848e65bbc129060bbbbefdcf79694e1d335
Daniel <dwimmer1@htl-steyr.ac.at>
2022-03-10 08:46:20 +0100

    dwimmer1 Datein

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -6,6 +6,11 @@ import java.util.Date;
 @autor  :  dwimmer1
  */
 
+/*
+Klasse: Person
+In der Klasse werden die Daten zu jeweiligen Personen initialisiert
+ */
+
 public class Person {
 
     String name;
@@ -14,15 +19,26 @@ public class Person {
     Date birthday;
     PhoneNumber phone;
 
-
-
+    /**
+     * Funktionsbeschreibung
+     * public Person dient als Konstruktor
+     * @param    n String für den namen
+     * @param    gn String für givename
+     * @param    bd geburtsdatum
+     * @param    p Telefonnummer
+     * @return 	void
+     */
     public Person(String n, String gn, Date bd, PhoneNumber p) {
         this.name = n;
         this.givename = gn;
         this.birthday = bd;
         this.phone = p;
     }
-
+    /**
+     * Funktionsbeschreibung
+     * toString
+     * @return 	String
+     */
     @Override
     public String toString() {
         return "Person{" +
--- src/com/company/StreamOperation.java
+++ src/com/company/StreamOperation.java
@@ -1,21 +1,44 @@
 package com.company;
 
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
+import java.util.ArrayList;
 
 /*
 @autor  :  dwimmer1
+     * @param    n String für den namen
+     * @param    gn String für givename
+     * @param    bd geburtsdatum
+     * @param    p Telefonnummer
  */
 
 public class StreamOperation {
 
+    ArrayList<String> alist = new ArrayList<>();
+    public void ToStream(FileWriter w, String line){
 
-    public void ToStream(Writer w, Person p){
+        try{
+            String[] array = line.split(";");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
     }
-    public void fromStream(Reader r, Person p){
+    public String fromStream(Reader r){
+            String b = "";
+        try {
+            BufferedReader in = new BufferedReader(r);
+            for (int i = 0; i < in.read(); i++) {
+                System.out.println(in.readLine());
+                b = in.readLine();
 
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return b;
 
     }
 }
